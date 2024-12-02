28c39681359d8df2d0b3c9256d6e4d0db0409a45
Jakobsito <jakobsito04@gmail.com>
2024-12-01 23:21:16 -0800

    Decryption function started

--- main.cpp
+++ main.cpp
@@ -3,23 +3,24 @@
 #include <string>
 using namespace std;
 
-void encyrpt(string key, string msg);
+void encyrpt(string, string);
+void decrypt(string, string);
 
 int main() {
     string key = "hi";
-    string msg = "ANything";
+    string msg = "HVfboquo";
 
-    encyrpt(key, msg);
+    decrypt(key, msg);
 }
 
 void encyrpt(string key, string msg) {
     string result = "";
+    char c, k, r;
     for (int i = 0; i < msg.size(); i++) {
-        char c = msg[i];
-        char k = key[i % key.size()];
-        char r;
+        c = msg[i];
+        k = key[i % key.size()];
         if (isupper(c)) {
-            r = ((c + ((k % 32) - 1)) - 64) % 26 + 64 ;
+            r = ((c + ((k % 32) - 1)) - 64) % 26 + 64;
         }
         else {
             r = ((c + ((k % 32) - 1)) - 96) % 26 + 96;
@@ -32,5 +33,18 @@ void encyrpt(string key, string msg) {
 }
 
 void decrypt(string key, string msg) {
-
+    string result = "";
+    char c, k, r;
+    for (int i = 0; i < msg.size(); i++) {
+        c = msg[i];
+        k = key[i % key.size()];
+        if (isupper(c)) {
+            r = ((c - ((k % 32) - 1)) - 64) % 26 + 64;
+        }
+        else {
+            r = ((c - ((k % 32) - 1)) - 96) % 26 + 96;
+        }
+        result.push_back(r);
+    }
+    cout << result;
 }
