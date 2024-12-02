238f539ee95d3a08845289d3c6d79ee0304bf48d
Cjoshua28 <tinyjoshua28@gmail.com>
2024-12-02 00:26:43 -0800

    Added code functionality

--- main.cpp
+++ main.cpp
@@ -3,27 +3,68 @@
 #include <string>
 using namespace std;
 
-void encyrpt(string, string);
+void encrypt(string, string);
 void decrypt(string, string);
 
 int main() {
-    string key = "hi";
-    string msg = "HVfboquo";
+    cout << "Greetings!";
 
-    decrypt(key, msg);
+    int operation = userInterface();
+    string key;
+    string msg;
+    switch (operation)
+    {
+    case 1:
+        cout << "Enter message to encrypt: ";
+        cin >> msg;
+        cout << "Enter key: ";
+        cin >> key;
+        encrypt(msg, key);
+        break;
+    case 2:
+        cout << "Enter message to decrypt: ";
+        cin >> msg;
+        cout << "Enter key: ";
+        cin >> key;
+        decrypt(msg, key);
+        break;
+    default:
+        break;
+    }
 }
 
-void encyrpt(string key, string msg) {
+int userInterface() {
+    int choice;
+    bool validation = false;
+    do {
+    cout << "1. Encryption" << "\n2. Decryption" << "\n3. Quit" << endl;
+    cout << "Please select your operation: ";
+    cin >> choice;
+    if (choice > 3 || choice < 1) {
+        cout << "That is not a valid choice.";
+    }
+    else {
+        validation = true;
+    }
+    }
+    while (!validation);
+
+    return choice;
+}
+
+
+
+void encrypt(string msg, string key) {
     string result = "";
     char c, k, r;
     for (int i = 0; i < msg.size(); i++) {
         c = msg[i];
         k = key[i % key.size()];
         if (isupper(c)) {
-            r = ((c + ((k % 32) - 1)) - 64) % 26 + 64;
+            r = ((c - 65) + ((k % 32) - 1)) % 26 + 65;
         }
         else {
-            r = ((c + ((k % 32) - 1)) - 96) % 26 + 96;
+            r = ((c - 97) + ((k % 32) - 1)) % 26 + 97;
         }
 
         result.push_back(r);
@@ -32,17 +73,17 @@ void encyrpt(string key, string msg) {
     cout << result;
 }
 
-void decrypt(string key, string msg) {
+void decrypt(string msg, string key) {
     string result = "";
     char c, k, r;
     for (int i = 0; i < msg.size(); i++) {
         c = msg[i];
         k = key[i % key.size()];
         if (isupper(c)) {
-            r = ((c - ((k % 32) - 1)) - 64) % 26 + 64;
+            r = ((c - 39) - ((k % 32) - 1)) % 26 + 65;
         }
         else {
-            r = ((c - ((k % 32) - 1)) - 96) % 26 + 96;
+            r = ((c - 71) - ((k % 32) - 1)) % 26 + 97;
         }
         result.push_back(r);
     }
